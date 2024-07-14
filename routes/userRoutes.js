8db103d9f2ce4033f0a1ff8b1a27f9dcad101e4d
aqib.ahmed <aqib.ahmed@samarth.ac.in>
2024-07-15 01:57:05 +0530

    token verification fixed3

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middlewares/')
+const verifyToken = require('../middlewares/tokenVerification')
 const userController = require('../controllers/userController')
 
 router.get('/details', verifyToken,userController.getDetails);
